58e60926ee72cac845fe2b514a8e7fde073fb51d
c-blythe <91218910+c-blythe@users.noreply.github.com>
2022-10-12 00:00:35 -0400

    Delete Test.rb

--- Test.rb
+++
@@ -1,9 +0,0 @@
-require_relative "./Entity.rb"
-require_relative "./Enemy.rb"
-require_relative "./EnemyCreator.rb"
-require_relative "./CombatUtils.rb"
-
-
-slime = EnemyCreator.slime
-puts(slime.base_xp)
-puts(slime.total_xp)
